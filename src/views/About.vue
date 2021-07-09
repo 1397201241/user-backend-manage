38904eb45a3aa741afba4f2da3b32f64ed07aa5e
in-twinkling-of-an-eye <1774079811@qq.com>
2021-07-09 16:09:45 +0800

    7.9

---
+++ src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about">
+    <h1>项目关于</h1>
+  </div>
+</template>
+<style lang="less">
+  .about{
+    font-family: 幼圆;
+  }
+</style>
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+// vue.config.js
+const path = require("path");
+module.exports = {
+
+  pluginOptions: {
+    "style-resources-loader": {
+      preProcessor: "less",
+      patterns: [path.resolve(__dirname, "src/global.less")]
+    }
+  }
+};
